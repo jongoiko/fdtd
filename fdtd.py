Fix finite difference signs

fdtd.py
@@ -94,25 +94,25 @@ class FDTD:
         iter_range = tqdm(range(n_iters)) if show_progress else range(n_iters)
         for _ in iter_range:
             self.vel_x -= self.vel_coef * (
-                torch.roll(self.pressure, 1, dims=0) - self.pressure
+                torch.roll(self.pressure, -1, dims=0) - self.pressure
             )
             self.vel_x *= self.solid_damping
             self.vel_y -= self.vel_coef * (
-                torch.roll(self.pressure, 1, dims=1) - self.pressure
+                torch.roll(self.pressure, -1, dims=1) - self.pressure
             )
             self.vel_y *= self.solid_damping
             self.vel_z -= self.vel_coef * (
-                torch.roll(self.pressure, 1, dims=2) - self.pressure
+                torch.roll(self.pressure, -1, dims=2) - self.pressure
             )
             self.vel_z *= self.solid_damping
 
             self.pressure -= self.pressure_coef * (
                 self.vel_x
-                - torch.roll(self.vel_x, -1, dims=0)
+                - torch.roll(self.vel_x, 1, dims=0)
                 + self.vel_y
-                - torch.roll(self.vel_y, -1, dims=1)
+                - torch.roll(self.vel_y, 1, dims=1)
                 + self.vel_z
-                - torch.roll(self.vel_z, -1, dims=2)
+                - torch.roll(self.vel_z, 1, dims=2)
             )
 
             self.pressure /= attenuation_dt